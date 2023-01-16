d8b16b6a29e5adea449fe4c5adfc60d7fefe7956
AndriiMurchk <andr.murchik@gmail.com>
2023-01-16 21:59:40 +0200

    На перевірку

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
-ww
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    
+    document.body.style.backgroundColor = getRandomHexColor();
+    startBtn.disabled = true;
+    
+  }, 1000);
+});
+
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  
+  
+});
+
+
