caa382eedd6aa1f36f0110050c0eb972de6d2384
AndriiMurchk <113796697+AndriiMurchk@users.noreply.github.com>
2023-01-17 22:50:31 +0000

    Deploying to gh-pages from @ AndriiMurchk/goit-js-hw-09@71e00e9fff6ec3c19323c13aa85ccc11ddbec236 🚀

--- 01-color-switcher.6d1838df.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let a=null;e.addEventListener("click",(()=>{a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,e.disabled=!0}),1e3)})),t.addEventListener("click",(()=>{clearInterval(a),e.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.6d1838df.js.map
